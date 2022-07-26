e85c74932c70058b7344a1287886993d855b5ce8
Vlad <vlad.melnik@bk.ru>
2022-07-26 20:23:11 +0300

    3 task added

---
+++ TASK-3/Program.cs
@@ -0,0 +1,35 @@
+﻿
+int[,] Spiral()
+{
+    int side = 4;
+    int sideelement = 1;
+    int[,] result = new int[side,side];
+    int num = 1;
+    for (int k = 0; k < side - sideelement * 2; k++)
+    {
+        for (int j = k; j < side - sideelement - k; j++, num++)
+            result[k,j] = num;
+        for (int i = k; i < side - sideelement - k; i++, num++)
+            result[i,side - sideelement - k] = num;
+        for (int j = side - sideelement - k; j >= k; j--, num++)
+            result[side - sideelement - k,j] = num;
+        for (int i = side - sideelement * 2 - k; i > k; i--, num++)
+            result[i,k] = num;
+    }
+    return result;
+}
+
+void Show2dArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j= 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+
+        Console.WriteLine();
+    }
+}
+
+
+int[,] myArray = Spiral();
+Show2dArray(myArray);
